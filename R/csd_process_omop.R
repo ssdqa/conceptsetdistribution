bc019492909ad6e83cfc3cbf1b85ced29ade5b4d
wieandk <wieandk@chop.edu>
2025-12-05 11:59:50 -0500

    apply changes from squba.gen

--- R/csd_process_omop.R
+++ R/csd_process_omop.R
@@ -90,7 +90,7 @@ csd_process_omop <- function(cohort,
 
   # Prep cohort
 
-  cohort_prep <- prepare_cohort(cohort_tbl = cohort_filter, age_groups = age_groups, codeset = NULL,
+  cohort_prep <- prepare_cohort(cohort_tbl = cohort_filter, age_groups = age_groups,
                                 omop_or_pcornet = 'omop') %>%
     group_by(!!! syms(grouped_list))
 
--- R/csd_process_pcornet.R
+++ R/csd_process_pcornet.R
@@ -86,7 +86,7 @@ csd_process_pcornet <- function(cohort,
 
   # Prep cohort
 
-  cohort_prep <- prepare_cohort(cohort_tbl = cohort_filter, age_groups = age_groups, codeset = NULL,
+  cohort_prep <- prepare_cohort(cohort_tbl = cohort_filter, age_groups = age_groups,
                                 omop_or_pcornet = 'pcornet') %>%
     group_by(!!! syms(grouped_list))
 
